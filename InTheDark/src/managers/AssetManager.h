d2fdebfccb3eeda2a41aaff8b4cd4eb18fbadf8f
bitThirteen <mich1.eickmeyer@gmail.com>
2023-05-03 14:33:38 +0200

    Created gameobj and moved relevant structs over, changed access to load methods in AssetManager to private

--- InTheDark/src/managers/AssetManager.h
+++ InTheDark/src/managers/AssetManager.h
@@ -7,31 +7,44 @@
 
 #include "../util/logger/loguru.hpp"
 #include "../util/util.h"
+#include "../objects/GameObject.h"
 
 typedef std::vector<glm::vec3> vec3v;
 typedef std::vector<glm::vec2> vec2v;
-
-enum Object
-{
-	Crate,
-	Stone
-};
-
-struct ObjData
-{
-	vec3v v;
-	vec2v uv;
-	vec3v n;
-};
 
 class AssetManager
 {
 public:
 	/**
-	 * @brief Loads an wavefront .obj file from a specified path.
+	 * @brief Returns an already loaded object as ObjData struct.
 	 * 
-	 * @param The path the .obj is located at
-	 * @return The .obj as an ObjData struct.
+	 * @param The object that should be returned. For possible object types, refer to the 'Object' enum in the GameObject header
+	 * @return The object data.
+	*/
+	ObjData getObj(Object);
+
+	/**
+	 * @brief Returns a reference to the AssetManager Singleton.
+	 * 
+	 * @return A reference to the AssetManager.
+	*/
+	static AssetManager& getInstance()
+	{
+		static AssetManager instance;
+		return instance;
+	};
+
+	AssetManager(AssetManager const&)	= delete;
+	void operator=(AssetManager const&) = delete;
+private:
+	AssetManager();
+
+	std::unordered_map<Object, ObjData> objects;
+
+	void loadAll();
+
+	/**
+	 * Loads a wavefront .obj file from a specified file path. This method will fail if the .obj is not triangulated.
 	*/
 	ObjData loadObj(std::string);
 
@@ -49,32 +62,4 @@ public:
 	 * TODO
 	*/
 	void loadAudio();
-
-	/**
-	 * @brief Returns an already loaded object as ObjData struct
-	 * 
-	 * @param The object that should be returned. For possible object types, refer to the local 'Object' enum
-	 * @return The object data
-	*/
-	ObjData getObj(Object);
-
-	/**
-	 * @brief Returns a reference to the AssetManager Singleton
-	 * 
-	 * @return A reference to the AssetManager
-	*/
-	static AssetManager& getInstance()
-	{
-		static AssetManager instance;
-		return instance;
-	};
-
-	AssetManager(AssetManager const&)	= delete;
-	void operator=(AssetManager const&) = delete;
-private:
-	AssetManager();
-
-	std::unordered_map<Object, ObjData> objects;
-
-	void loadAll();
 };
---
+++ InTheDark/src/objects/GameObject.h
@@ -0,0 +1,28 @@
+#pragma once
+
+enum Object
+{
+	Crate,
+	Stone
+};
+
+struct ObjData
+{
+	vec3v v;
+	vec2v uv;
+	vec3v n;
+};
+
+// TODO: This class is the C++ side of a GameObject containing the draw method.
+// To make this work in Lua, we will need some sort of wrapper between a C++ GameObject
+// and a Lua GameObject.
+class GameObject
+{
+public:
+	void draw()
+	{
+
+	}
+private:
+
+};
