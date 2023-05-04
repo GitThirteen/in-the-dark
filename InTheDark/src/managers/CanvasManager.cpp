d3fe221026a15fd50d76f0288571195bdfa31952
bitThirteen <mich1.eickmeyer@gmail.com>
2023-05-05 00:00:16 +0200

    CanvasManager::clear() now works as expected

--- InTheDark/src/managers/CanvasManager.cpp
+++ InTheDark/src/managers/CanvasManager.cpp
@@ -7,7 +7,7 @@ void CanvasManager::clear()
 
 void CanvasManager::clear(uint8_t r, uint8_t g, uint8_t b)
 {
-	clear(r, g, b, 1.0f);
+	clear(r, g, b, 255);
 }
 
 void CanvasManager::clear(uint8_t r, uint8_t g, uint8_t b, uint8_t alpha)
@@ -15,5 +15,6 @@ void CanvasManager::clear(uint8_t r, uint8_t g, uint8_t b, uint8_t alpha)
 	float rgb_r = r / 255.0f;
 	float rgb_g = g / 255.0f;
 	float rgb_b = b / 255.0f;
-	glClearColor(rgb_r, rgb_g, rgb_b, 1.0f);
+	float rgb_a = alpha / 255.0f;
+	glClearColor(rgb_r, rgb_g, rgb_b, alpha);
 }
