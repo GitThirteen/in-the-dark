1fe39537025a6e06db2af322a118b65ce178539d
bitThirteen <mich1.eickmeyer@gmail.com>
2023-05-12 22:45:51 +0200

    Minor refactor if else -> if

---
+++ InTheDark/src/managers/Clock.cpp
@@ -0,0 +1,30 @@
+#include "Clock.h"
+
+double Clock::getDeltaTime()
+{
+	return this->dt.count();
+}
+
+int Clock::getFPS()
+{
+	int fps = 1 / this->dt.count();
+	return std::max(0, fps);
+}
+
+void Clock::tick()
+{
+	this->cur_time = std::chrono::steady_clock::now();
+}
+
+void Clock::tock()
+{
+	if (first_frame)
+	{
+		this->dt = this->cur_time - this->cur_time;
+		this->first_frame = false;
+		return;
+	}
+
+	this->dt = this->cur_time - this->last_time;
+	this->last_time = this->cur_time;
+}
